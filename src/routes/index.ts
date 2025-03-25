914194891d150e137d1d7fb713c9dade1207e7a9
Vinaykv18 <vinaykv380@gmail.com>
2025-03-25 09:52:12 +0530

    Implement user signup endpoint with JWT token generation and user existence check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,20 @@
 import { Hono } from "hono";
-
+import { signUpWithUsernameAndPassword } from "../controllers/authentication";
 export const hono = new Hono();
+
+hono.get("/authetication/signup", async (context) => {
+    const { username, password } = await context.req.json();
+    try {   
+        const result = await signUpWithUsernameAndPassword({ username, password });
+        return context.json({
+            data: result
+        }, 
+        201);
+    } catch (e) {
+        return context.json({message:"Error"}, 500);
+    }
+
+});
 
 hono.get("/health", (context) => {
     return context.json(
